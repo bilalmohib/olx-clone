c1638a16ffc25395ef4d87b4f63a39f847461a49
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2021-06-21 10:27:57 +0500

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,14 +4,7 @@ import 'firebase/database'
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyDAelHByyhxwHlF7JIeCCGwL5HP622k5E8",
-    authDomain: "fastsellat.firebaseapp.com",
-    databaseURL: "https://fastsellat.firebaseio.com",
-    projectId: "fastsellat",
-    storageBucket: "fastsellat.appspot.com",
-    messagingSenderId: "717565114992",
-    appId: "1:717565114992:web:971b1dd5199c57b04812b5",
-    measurementId: "G-0NRD11FNQZ"
+   
   };
   
   // Initialize Firebase
